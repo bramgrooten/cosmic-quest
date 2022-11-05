615e85eb1b3d20daef7d9e0e477ab652f7ff0e8b
Bram Grooten <b.j.grooten@student.tue.nl>
2022-11-05 12:23:29 +0100

    new planet properties, for Bas' system

--- stars.py
+++ stars.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Star:
@@ -10,7 +11,7 @@ class Star:
 # diameter of milky way galaxy: 100,000 light years. Center is (0,0)
 def generate_star():
     # sample from Gaussian distribution
-    r = np.random.normal(25_000, 5_000)
+    r = np.random.normal(25_000, 7_000)
     angle = np.random.uniform(0, 2*np.pi)
     if r > 100_000:  # get r back in range
         r = 100_000 - (r - 100_000)
@@ -18,3 +19,19 @@ def generate_star():
     x = r * np.cos(angle)
     y = r * np.sin(angle)
     return x, y
+
+
+
+
+
+if __name__ == '__main__':
+
+    stars = []
+    for i in range(2000):
+        x, y = generate_star()
+        stars.append((x, y))
+
+    # make a scatter plot of the stars
+    x, y = zip(*stars)
+    plt.scatter(x, y)
+    plt.show()
