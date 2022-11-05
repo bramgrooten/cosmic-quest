9c4b3363722d5802f2ee6c0bc6c7072b4dd60b48
Daan <schalk.Daan@hotmail.com>
2022-11-05 20:16:40 +0100

    app test

--- app.py
+++ app.py
@@ -10,17 +10,26 @@ galaxy_map = map_generator.generate()
 
 @app.route("/init_galaxy")
 def init_galaxy():
+    global galaxy_map
+    
+    galaxy_map = map_generator.generate()
     return map_generator.save_map_to_json(galaxy_map)
 
 
 @app.route("/move")
 def move():
     # get recommendations for next planet to colonize
-    planet_scores = recommend(galaxy_map)
+    planet_scores, best_candidate, closest_colony = recommend(galaxy_map)
 
     # add the best planet to human colony
-    new_planet_index = np.argmax(planet_scores)
-    galaxy_map.human_colony.append(new_planet_index)
+    #new_planet_index = np.argmax(planet_scores)
+    #galaxy_map.human_colony.append(new_planet_index)
+    #TODO: add old new's to full list
+
+    galaxy_map.new_connections = [(closest_colony, best_candidate)]
+    galaxy_map.new_human_colony_planets = [best_candidate]
+
+    
 
     # return the new human colony to frontend
     map_generator.save_map_to_json(galaxy_map)
