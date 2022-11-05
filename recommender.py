2f224977691787c561cda2800c0e9a54638ef714
DukeBas <35384117+DukeBas@users.noreply.github.com>
2022-11-05 17:15:03 +0100

    simple scores

--- recommender.py
+++ recommender.py
@@ -1,3 +1,30 @@
+import json
+
+# import from calc
+from calc.planet_score import planet_score
+
 # takes galaxy state and returns best option
-def recommend():
-  pass
+def recommend(map):
+  planets = map['planet_list']
+  scores = []
+  
+  # calculate scores for the planets
+  for i in range(len(planets)):
+        p = planets[i]
+        scores.append(planet_score(min([0]), p['radius'], p['mass'], p['temperature']))
+  
+  # get the planet with the best score
+  index_best = 0
+  best_score = 0
+  for i in range(len(planets)):
+    if (scores[i] > best_score):
+        best_score = scores[i]
+        index_best = i
+        
+  return scores, planets[index_best]
+
+f = open("test.json")
+map = json.load(f)
+
+# recommend(map)
+print(recommend(map))
