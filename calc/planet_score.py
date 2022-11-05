376b44472b574883abe371b42018470f5a1831d2
DukeBas <35384117+DukeBas@users.noreply.github.com>
2022-11-05 17:15:03 +0100

    simple scores

--- calc/planet_score.py
+++ calc/planet_score.py
@@ -1,10 +1,10 @@
-import ESI
+from calc.ESI import calc_esi
 
 # calculates a score for a planet based on it's characteristics
 # score is based on planet habitability and distance
 def planet_score(dist, rad, mass, temp):
   # ESI is a measure related to habitability
-  ESI = ESI(rad, mass, temp)
+  ESI = calc_esi(rad, mass, temp)
   
   # closer is better
   dist = max(100 - dist**1.2, 0)
