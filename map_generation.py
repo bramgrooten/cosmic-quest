59f3f2100d88afe2b559e84112a070a1e2934627
Bram Grooten <b.j.grooten@student.tue.nl>
2022-11-05 17:03:41 +0100

    add first human colony planet

--- map_generation.py
+++ map_generation.py
@@ -34,6 +34,10 @@ class map_generation:
                 # Can definitely be made more elegant, simple solution for now
                 Map.dist_map[x][y] = math.dist([Map.planet_list[x].x, Map.planet_list[x].y], [Map.planet_list[y].x, Map.planet_list[y].y]) #math.sqrt(pow(x,2) + pow(x,2))  abs(Map.planet_list[x] - Map.planet_list[y])
 
+    def init_human_colony(self):
+        first_planet_index = random.randint(0, len(Map.planet_list))
+        Map.human_colony.append(first_planet_index)
+
     def save_map_to_json(self, map):
         star_list = []
         planet_list = []
@@ -51,6 +55,7 @@ class map_generation:
         json_map = {
             "star_list": star_list,
             "planet_list": planet_list,
+            "human_colony": map.human_colony,
         }
         with open("map.json", "w") as outfile:
             json.dump(json_map, outfile)
@@ -60,19 +65,22 @@ class map_generation:
         # determine how many stars we need
         star_count = 2000
         # determine where the stars are
-        self.generate_milkyway_distribution(self, star_count)
+        self.generate_milkyway_distribution(star_count)
 
         # for each star...
         for i, star in enumerate(Map.star_list):
             # determine how many planets
             planet_count = 3#random.randint(1, 5)
             # for each star, determine where planets are
-            self.generate_star_system_distribution(self, star, planet_count)
+            self.generate_star_system_distribution(star, planet_count)
 
         # determine distances between planets
-        self.determine_distances(self)
+        self.determine_distances()
+
+        # determine where the human colony starts
+        self.init_human_colony()
 
         # save the map to json
-        self.save_map_to_json(self, Map)
+        self.save_map_to_json(Map)
         
 
