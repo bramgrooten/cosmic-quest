6f1db2f292a073575d6e2bb23494e4781fd3acaa
Daan <schalk.Daan@hotmail.com>
2022-11-05 21:35:01 +0100

    does anyone even read these messages?

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from map_generation import map_generation
 if __name__ == "__main__":
     # generate and fill in nr_of_profiles profiles
     mg = map_generation
-    #map_generation.generate()
+    
     mg.generate(mg)
