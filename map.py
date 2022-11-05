64f1a00f4430c8109c442bb020554bd968cd8899
Daan <schalk.Daan@hotmail.com>
2022-11-05 18:33:54 +0100

    extend map struct

--- map.py
+++ map.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Map:
-    def __init__(self, newTemperature):
-        temperature = newTemperature
-        pass
-
     star_list = []
     planet_list = []
     dist_map = []
     human_colony = []
+    connections = []
+
+    new_human_colony_planets = []
+    new_connections = []
