1a687f624e1135524532162e6903879ed0537aa6
Daan <schalk.Daan@hotmail.com>
2022-11-05 10:28:38 +0100

    merge conflict test

---
+++ map.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Map:
+    def __init__(self, newTemperature):
+        temperature = newTemperature
+        pass
+
+    planet_list = []
+    dist_map = []
--- map_generation.py
+++ map_generation.py
@@ -1,8 +1,27 @@
 from temp_planet import Planet
+from map import Map
+
+nr_of_planets = 4
 
 class map_generation:
+    def determine_distances():
+        Map.dist_map = [[-1]*nr_of_planets]*nr_of_planets
+        for x in range(nr_of_planets):
+            for y in range(nr_of_planets):
+                #TODO
+                Map.dist_map[x][y] = -1
+
     def generate():
-        # generate planets
-        p = Planet(newTemperature=0)
-        print(p.temperature)
-        #return p
+        # determine how many stars we need
+
+        # determine where the stars are
+
+        # for each star, determine how many planets
+        
+        # for each star, determine where planets are
+
+        # configure planets
+
+        # determine distances between planets
+        determine_distances()
+
