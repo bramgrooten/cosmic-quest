9bd39cb5e0d1bc8a76b3dace379217ee566b01ab
Bram Grooten <b.j.grooten@student.tue.nl>
2022-11-05 19:58:38 +0100

    galaxy_map rename

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from flask import Flask
 from map_generation import map_generation
 import numpy as np
+from recommender import recommend
 
 app = Flask(__name__)
 
-map2 = map_generation()
-map = map2.generate()
+map_generator = map_generation()
+galaxy_map = map_generator.generate()
 
 @app.route("/init_galaxy")
 def init_galaxy():
-    return map2.save_map_to_json(map)
+    return map_generator.save_map_to_json(galaxy_map)
 
 
 @app.route("/move")
 def move():
     # get recommendations for next planet to colonize
-    planet_scores = recommender.get_planet_scores()
+    planet_scores = recommend(galaxy_map)
 
-    # add best planet to human colony
+    # add the best planet to human colony
     new_planet_index = np.argmax(planet_scores)
     galaxy_map.human_colony.append(new_planet_index)
 
     # return the new human colony to frontend
-    galaxy_map.save_map_to_json(galaxy_map)
+    map_generator.save_map_to_json(galaxy_map)
 
 
 
